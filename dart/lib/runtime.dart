6b3668aca7709e557de48138ca7d4b7bef8d930a
Frenco <hey@frenco.dev>
2021-02-05 14:17:28 +0630

    feat: use handler directly from shelf

--- dart/lib/runtime.dart
+++ dart/lib/runtime.dart
@@ -1,21 +1,17 @@
-import 'package:aws_lambda_dart_runtime/aws_lambda_dart_runtime.dart'
-    show Runtime, InvocationResult, Handler;
-
-import 'package:shelf/shelf.dart' show Request, Response;
+import 'package:aws_lambda_dart_runtime/aws_lambda_dart_runtime.dart' as aws;
+import 'package:shelf/shelf.dart' as shelf;
 
 import 'package:vercel_dart/src/models/event.dart';
 import 'package:vercel_dart/src/utils/response_to_object.dart';
 
-typedef Lambda = Future<Response> Function(Request);
-
-void handle(Lambda handler, {String name = 'bootstrap'}) {
+void handle(shelf.Handler handler, {String name = 'bootstrap'}) {
   // ignore: prefer_function_declarations_over_variables
-  final Handler<Event> _handler = (context, event) async {
+  final aws.Handler<Event> _handler = (context, event) async {
     final res = await handler(event.body);
-    return InvocationResult(context.requestId, await responseToObject(res));
+    return aws.InvocationResult(context.requestId, await responseToObject(res));
   };
 
-  Runtime()
+  aws.Runtime()
     ..registerEvent<Event>((Map<String, dynamic> json) => Event.fromJson(json))
     ..registerHandler<Event>(name, _handler)
     ..invoke();
